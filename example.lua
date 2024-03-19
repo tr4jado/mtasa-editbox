a19bbff48abfc2ded254b783a3706e6342c62e59
tr4jado <72276369+tr4jado@users.noreply.github.com>
2024-03-19 20:44:34 -0300

    Update example.lua

--- example.lua
+++ example.lua
@@ -7,5 +7,7 @@ local editbox = Editbox.new({
 
 addEventHandler('onClientRender', root, function()
     dxDrawRectangle(256, 313, 277, 60, tocolor(0, 0, 0, 250))
+    editbox:setProperties('parent', {256, 313, 277, 60)
+
     editbox:draw('Clique aqui para digitar', 276, 332, 237, 21, tocolor(255, 255, 255))
 end)
